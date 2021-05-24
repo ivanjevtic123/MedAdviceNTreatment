9ef0b5cdac0df7fc19ac5c9c61235759f0c51c81
Filip Zaric <filip.zaric1234@gmail.com>
2021-05-24 11:10:36 +0200

    Dodate funkcionalnosti dodavanja kartona i odobravanje zahteva

---
+++ Faza5/app/Views/stranice/zakazivanjeTermina.php
@@ -0,0 +1,52 @@
+        <!-- Right Div -->
+        <div class="col-lg-4 col-md-4 offset-2 right">
+               <br>
+               <br>
+<table class="table table-light table-striped" cellspacing="15px">
+    <tr>
+        <td>Datum termina:</td>
+        <td>
+            <input type="date" id="dateOfExam">
+        </td>
+        </tr>
+        <tr>
+        <td>Vreme termina:</td>
+        <td>
+            <select name="timeOfExam" id="fromTimeOfExam">
+                <option value="1">1:00</option>
+                <option value="2">2:00</option>
+                <option value="3">3:00</option>
+                <option value="4">4:00</option>
+                <option value="5">5:00</option>
+                <option value="1">6:00</option>
+                <option value="2">7:00</option>
+                <option value="3">8:00</option>
+                <option value="4">9:00</option>
+                <option value="5">10:00</option>
+                <option value="1">11:00</option>
+                <option value="2">12:00</option>
+                <option value="3">13:00</option>
+                <option value="4">14:00</option>
+                <option value="5">15:00</option>
+                <option value="5">16:00</option>
+                <option value="5">17:00</option>
+                <option value="5">18:00</option>
+                <option value="5">19:00</option>
+                <option value="5">20:00</option>
+                <option value="5">21:00</option>
+                <option value="5">22:00</option>
+                <option value="5">23:00</option>
+                <option value="5">24:00</option>
+              </select>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" align="center"> <button class="btn btn-secondary">Zakazi termin</button> </td>
+        
+    </tr>
+</table>
+
+
+
+            </div>
+        </div>
