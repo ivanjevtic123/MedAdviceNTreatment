d407ce7fe4892e637bd0dc975e7c29baa758a899
ivanjevtic <ji180550d@student.etf.bg.ac.rs>
2021-06-05 08:47:49 +0200

    Dodat Korisnik-filter

---
+++ Faza5/app/Filters/KorisnikFilter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class KorisnikFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        // $session=session();
+        // if(!$session->has('korisnik')){
+        //     return redirect()->to(site_url('Gost'));
+        // }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // Do something here
+    }
+}
